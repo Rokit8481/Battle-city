a113251f83fc3d528c16aeb82f8e78374b1c53c8
Rokit8481 <162879025+Rokit8481@users.noreply.github.com>
2025-05-03 14:00:20 +0300

    Add files via upload

---
+++ класи/level1.py
@@ -0,0 +1,15 @@
+level_map = [
+    "WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW",
+    "W       W     G     W        LW",
+    "W WWWW WWW WWWWWWW WWW WWWWWW W",
+    "W W        W   W       W     TW",
+    "W W WWWWWW W W W WWWWWWW WWWW W",
+    "W W      W   W   W     W      W",
+    "W WWWWWW WWWWWWWWWWWWWWWWWWWW W",
+    "W      W     D     W    S     W",
+    "W WWWW WWW WWWWWWW WWW WWWWW WW",
+    "W W    W   W     W   W      W W",
+    "W W WWWWW WWWWWWW WWW WWWWW W W",
+    "W   T     G     L     D     G W",
+    "WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW"
+]
